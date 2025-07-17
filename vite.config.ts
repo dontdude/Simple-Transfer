cb3b4d8b216432061acfba4b3e809049995bb915
Chandan Mishra <imchandan1947@gmail.com>
2025-07-17 13:33:26 +0530

    feat: api proxy using vite for cors issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Any request to /api will be forwarded to the backend 
+      '/api': {
+        target: 'http://localhost:8860',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
